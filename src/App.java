fd32cc7b07079bb66b37136fdab3b53cb9e7e5ad
NygerianPrynce <fadhillawal06@gmail.com>
2022-11-08 14:08:52 -0600

    commt

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, if");
+        System.out.println("Hello, if");
+        System.out.println("Hello, cock");
+
     }
 }
