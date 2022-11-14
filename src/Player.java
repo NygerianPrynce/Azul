b7558a193af5499cac7d644a426f9ad485d9c66f
Maya Ali <k1209886@students.katyisd.org>
2022-11-14 13:28:03 -0600

    //adding to the player class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,14 @@
 public class Player {
+
+private int playerNumber; 
+private boolean isStarter;
+private PlayerWall pW;
+private PlayerLine pL; 
+
+public Player(){
+    pW = new PlayerWall();
+    pL = new PlayerLine(); 
+}
+
     
 }
