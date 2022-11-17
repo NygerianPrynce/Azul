4673b9041b2f859369cd98abd5afdf51a8f758e3
Maya Ali <k1209886@students.katyisd.org>
2022-11-17 13:00:50 -0600

    teszt

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,14 @@
 public class Player {
 
 private int playerNumber; 
-private boolean isStarter;
+private boolean isStarterr;
 private PlayerWall pW;
 private PlayerLine pL; 
 
 public Player(){
     pW = new PlayerWall();
     pL = new PlayerLine(); 
+
 }
 
     
